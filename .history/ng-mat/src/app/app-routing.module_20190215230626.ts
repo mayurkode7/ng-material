1b9fe8d8c3d205e75e15a583947acb77066fe80d
Mayur Kode <mayur.kode@outlook.com>
2019-02-15 23:11:49 -0500

    footer and page not found adds

--- .history/ng-mat/src/app/app-routing.module_20190215230626.ts
+++ .history/ng-mat/src/app/app-routing.module_20190215230626.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SharedComponent } from './shared/shared.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'shared', component: SharedComponent },
+  { path: '', redirectTo: '/shared', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ng-mat/src/app/app-routing.module.ts
+++ ng-mat/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedComponent } from './shared/shared.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'shared', component: SharedComponent }
+  { path: 'shared', component: SharedComponent },
+  { path: '', redirectTo: '/shared', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
